b2c4d3e521a73b59063f5718ee304dfd50ec31bd
srujan <vaidyasrujan@gmail.com>
2025-08-17 15:14:45 +0530

    basic changes for parking and exit in api

--- park/api/views.py
+++ park/api/views.py
@@ -1,41 +1,66 @@
+from gc import get_objects
+
+from django.http import Http404
 from django.shortcuts import render
 from django.template.context_processors import request
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics,mixins
-from .serializers import PlateSerializer,Plate1Serializer
+from .serializers import PlateSerializer,PlateInputSerializer
 from .models import Park
 from .sqltest import Server
 from .parking import Parkings
 
 
 class Parkingsys(APIView):
-    def obj(self):
 
-        server = Server()
-        raw = server.find()
-        final = [
-            {"id": row[0], "plate": row[1]} for row in raw
-        ]
-        return final
     def get(self,request):
-        data=self.obj()
-        serializer = PlateSerializer(data,many=True)
+        park=Park.objects.all()
+        serializer=PlateSerializer(park,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
 
+class Parks(APIView):
+
+    def get(self, request):
+        park = Park.objects.all()
+        serializer = PlateSerializer(park, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self,request):
-        serializer=Plate1Serializer(data=request.data)
+        serializer= PlateInputSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            p = Parkings()
+            p=Parkings()
             data=request.data
-            plateno=data['plate']
-            #print(plateno)
-            p.park(plateno)
+            plate=data['carno']
+            p.park(plate)
             return Response(serializer.data,status=status.HTTP_200_OK)
-        print(serializer.errors)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class Exit(APIView):
+
+    def get(self, request):
+        park = Park.objects.all()
+        serializer = PlateSerializer(park, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self,request):
+        serializer=PlateInputSerializer(data=request.data)
+        if serializer.is_valid():
+            p=Parkings()
+            data=request.data
+            plate=data['carno']
+            p.depart(plate)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
 
 
 
